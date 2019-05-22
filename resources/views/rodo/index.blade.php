f97b5f13cee2d170e29d57fb7a19ffaa4032b8cb
Klaudiusz oleksiuk <klaudek@wop.pl>
2019-05-22 11:27:35 +0200

    rodo

---
+++ resources/views/rodo/index.blade.php
@@ -0,0 +1,81 @@
+@extends('app.default.one_col')
+
+
+
+@section('default_content_text')
+
+
+
+	<div id="app">
+
+	<div id="section_kontakt" class="row mt-5" style="min-height: 65vh">
+
+		<div class="offset-md-2"></div>
+
+		<div class="col-md-8 col-sm-12">
+
+	
+		    <div class="row" style="margin-top: 50pt;">
+				<div class="col-md-12 col-sm-12">
+				    <p class="header-text ">Polityka prywatności i coocies serwisu Kamień Design</p>
+				    <div class="underline my-3"></div>	
+
+
+					<div class="alert" variant="success" fade class="my-3"> 
+
+						<p>
+							<strong>Szanujemy Twoją prywatność i dbamy o bezpieczeństwo Twoich danych, dlatego prezentujemy Ci dokument, w którym znajdziesz zasady przetwarzania danych osobowych oraz wykorzystywania plików cookies i innych technologii śledzących w związku z korzystaniem z witryny www.kamiendesign.pl.</strong>
+						</p>
+
+						<hr class="mb-3 mt-3">
+
+						<p>
+							W związku z tym, że od 25 maja 2018 roku obowiązuje Rozporządzenie Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (znane jako GDPR lub RODO), informujemy Cię, w jaki sposób dbamy o Twoje dane. 
+						</p>
+
+						<p>
+							Administratorem witryny i gromadzonych w niej danych jest SVPR MACIEJ BRZESKI KAMIEŃ-DYSTRYBUCJA z siedzibą w Poznaniu, ul. Mickiewicza 30/3, NIP: 8451829153 REGON: 519546800. Ochrona danych odbywa się zgodnie z wymogami powszechnie obowiązujących przepisów prawa, a ich przechowywanie ma miejsce na zabezpieczonych serwerach.
+						</p>
+
+						<h6>Dla twojej wygody przedstawiamy informacje skrócone:</h6>
+
+						<ol>
+							
+							<li>Przetwarzamy Twoje dane osobowe, które gromadzimy w związku z korzystaniem przez Ciebie z naszego witryny i jej  funkcji: formularz kontaktowy, formularz zamówienia , zapis do newslettera.</li>
+							<li>Pośród danych osobowych, które przetwarzamy, mogą znajdować się, w szczególności, następujące informacje: imię i nazwisko, adres e-mail, numer telefonu, adres wykonywania planowanej inwestycji, nazwa firmy, adres IP.</li>
+							<li>Dokładamy wszelkich starań, by Twoje dane osobowe pozostały bezpieczne i nie trafiły w niepowołane ręce. Powierzamy przetwarzanie Twoich danych osobowych tylko sprawdzonym i zaufanym podmiotom świadczącym usługi związane z przetwarzaniem danych osobowych.</li>
+							<li>Przekazujemy Twoje dane osobowe do państw trzecich w związku z korzystaniem z narzędzi, które przechowują dane osobowe na serwerach zlokalizowanych w państwach trzecich, w szczególności w USA. Dostawcy tych narzędzi gwarantują odpowiedni poziom ochrony danych osobowych poprzez stosowne mechanizmy zgodności przewidziane przez RODO.</li>
+							<li>Zapewniamy Ci możliwość realizacji Twoich uprawnień wynikających z RODO, związanych z przetwarzaniem przez nas Twoich danych osobowych.</li>
+							<li>Śledzimy i analizujemy Twoje zachowania podejmowane w ramach naszej witryny  w celach statystycznych oraz optymalizacji działania naszej witryny</li>
+							<li>Wykorzystujemy narzędzia funkcjonujące w oparciu o pliki cookies, zarówno własne, jak i pliki cookies podmiotów trzecich. W ramach tych narzędzi nie mamy dostępu do informacji, które pozwalałaby nam na identyfikację Twojej osoby. Są to różnego rodzaju narzędzia analityczne, marketingowe, społecznościowe i komunikacyjne. Część z tych narzędzi wiąże się z zaimplementowaniem specjalnych skryptów śledzących.</li>
+							<li>Dajemy Ci możliwość zarządzania ustawieniami plików cookies bezpośrednio z poziomu naszej strony. Możesz w ten sposób włączać i wyłączać poszczególne narzędzia oraz odpowiadające im pliki cookies. Możesz również zarządzać ustawieniami plików cookies z poziomu przeglądarki internetowej lub instalując specjalne dodatki pozwalające na kontrolę nad plikami cookies.</li>
+							<li>W ramach zawartości witryny mogą znajdować się linki do witryn zewnętrznych zarządzanych przez podmioty trzecie.  Staramy się dobierać zaufane podmioty, ale nie ponosimy odpowiedzialności za zasady przetwarzania danych osobowych i wykorzystywania plików cookies przez operatorów witryn zewnętrznych. Szczegółów w tym zakresie możesz szukać w politykach prywatności witryn zewnętrznych.</li>
+							<li>Strona przechowywana jest na serwerze, który generuje logi. W logach przechowywane są informacje takie jak adres IP, data i czas serwera, informacje o przeglądarce internetowej i systemie operacyjnym. Logi służą wyłącznie celom operacyjnym i technicznym.</li>
+						</ol>
+
+						<p><strong>To tylko najważniejsze informacje! Zachęcamy Cię do pobrania i zapoznania się z umieszczonym poniżej  szczegółowym omówieniem naszej polityki prywatności.</strong></p>
+
+						<h5>Polityka prywatności - <a href="{{ asset('storage/pp/polityka_prywatnosci_kamiendesign.pdf') }}">pobierz [pdf]</a></h5>
+
+					</div>
+					<p>
+						
+					</p>
+
+				</div>
+		    </div>
+
+
+
+		</div>
+
+	</div>
+	
+
+	</div>
+
+
+
+
+
+@stop
