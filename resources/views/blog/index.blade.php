8c5a3b0111dca597192603eac7ef97b132a7bbd9
Klaudiusz Oleksiuk <klaudek@wop.pl>
2019-05-23 07:08:48 +0200

    form instal

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -16,6 +16,32 @@
 	}
 
 
+	#slot_1 {
+		position: relative;
+		/*background-image: url('images/blog/pu5.jpg');*/
+	}
+
+	#slot_1 .image {
+		width: 100%;
+	}
+
+	.overlay {
+		position: absolute;
+		bottom: 0; 
+		background: rgb(0, 0, 0);
+		background: rgba(0, 0, 0, 0.5); /* Black see-through */
+		color: #f1f1f1; 
+		width: 100%;
+		transition: .5s ease;
+		opacity: 1;
+		color: white;
+		font-size: 20px;
+		padding: 20px;
+		text-align: center;		
+	}
+
+
+
     </style>
 
 
@@ -31,11 +57,34 @@
 
 	    	<div class="mt-5">
 
-	    		<div class="d-flex flex-row  p-3 bg-secondary">
+	    		<div class="d-flex flex-row bg-secondary" style="">
 
-	  				<div class="col-md-6 p-2 bg-info">Flex item 1</div>
-	  				<div class="col-md-3 p-2 bg-warning">Flex item 2</div>
-	  				<div class="col-md-3 p-2 bg-primary">Flex item 3</div>	    		
+	  				<div id="slot_1" class="p-0 m-2 bg-info d-flex align-items-stretch" style="flex: 50%;">
+	  					<img src="{{ asset('images/blog/pu5.jpg') }}" class="image">
+	  					<div class="overlay">Tytuł artykułu</div>
+	  				</div>
+	  				
+	  				<div class="p-0 m-2 bg-warning" style="flex: 25%">
+	  					<div class="bg-primary m-2" style="height: 200pt;">
+	  						col 1	
+	  					</div>
+
+	  					<div class="bg-primary m-2" style="height: auto;">
+	  						col 2ś
+	  					</div>
+
+	  				</div>
+	  				
+	  				<div class="p-0 m-2 bg-info" style="flex: 25%">
+	  					<div class="bg-primary m-2" style="height: 300pt">
+	  						col 1	
+	  					</div>
+
+	  					<div class="bg-primary m-2" style="height: auto;">
+	  						col 2ś
+	  					</div>
+	  					
+	  				</div>	    		
 
 				</div>
 
